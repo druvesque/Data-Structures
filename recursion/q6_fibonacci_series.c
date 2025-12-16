e98d0597b9c5a5811e39dcec52c5b87d5b6c5745
druvesque <dhruvsachdeva6480@gmail.com>
2025-12-16 20:44:44 +0530

    recursion

--- recursion/q6_fibonacci_series.c
+++ recursion/q6_fibonacci_series.c
@@ -1,9 +1,30 @@
 #include<stdio.h>
 
+void fibonacci(int);
+static int a = 0;
+static int b = 1;
+
 int main() {
     int num;
     printf("Enter number of terms: ");
     scanf("%d", &num);
-
+    fibonacci(num);
     return 0;
+}
+
+void fibonacci(int n) {
+    if (n == 1) {
+        printf("Fibonacci Series: %d", a);
+        return;
+    }
+    if (n == 2) {
+        printf("Fibonacci Series: %d\t%d", a, b);
+        return;
+    }
+    fibonacci(n-1);
+    int next_term = a + b;
+    a = b;
+    b = next_term;
+    printf("\t%d", b);
+
 }
